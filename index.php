a7f2e540c8339b229477a376a16b94c35bec33ed
Ingrid <Kelder>
2014-09-23 15:15:16 +0300

    continue, break

--- index.php
+++ index.php
@@ -5,6 +5,25 @@
 <title>Harjutus 8</title>
 </head>
 <body>
+<h2>Funktsioon "continue"</h2>
+<?php 
+for ($number = 1; $number <= 10; $number++) {
+    if ($number == 5) {
+        continue; // Kui $nr == 5, siis jääb alljärgnev vahele ning tsükkel käivitub uuesti.
+    }
 
+    echo $number . ", ";
+}
+?>
+
+<h2>Funktsiooni break</h2>
+<?php 
+    for ($number = 1; $number <= 10; $number++) {
+        if ($number == 6) {
+            break;
+        }
+        echo $number;
+    }
+?>
 </body>
 </html>
